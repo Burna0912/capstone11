eb2b27eb91b1452a8a56a3e3bb1f8665542b5ed0
Burna0912 <dykai2000@gmail.com>
2025-06-04 11:41:22 +0900

    .

--- utils/transforms.py
+++
@@ -1,9 +0,0 @@
-# utils/transforms.py
-from torchvision import transforms
-
-def get_transforms():
-    return transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ConvertImageDtype(torch.float32),
-        transforms.Normalize([0.5], [0.5])
-    ])
